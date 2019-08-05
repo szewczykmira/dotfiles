f3ec96e293e9eaaac3977c98172b24070c4c6176
Mira <szewczyk.mira@gmail.com>
2019-08-05 22:15:20 +0200

    Remove linux-related files

--- misc/lein_profiles.clj
+++
@@ -1,9 +0,0 @@
-{:user  {:plugins  [[lein-vanity  "0.2.0"]
-                    [lein-try  "0.4.3"]
-                    [lein-checkall  "0.1.1"]
-                    [lein-ancient  "0.6.7"]
-                    [cloc  "0.1.0"]
-                    [cider/cider-nrepl "0.9.1"]
-                    [mvxcvi/whidbey  "1.0.0"]
-                    [acyclic/squiggly-clojure "0.1.3-SNAPSHOT"]
-                    [lein-plz  "0.4.0-SNAPSHOT" :exclusions  [[rewrite-clj]  [ancient-clj]]]]}}
